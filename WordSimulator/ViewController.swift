ef61879fafaadc378c30e50f4e9bb8851828913c
gdking137 <gdkim137@gmail.com>
2022-11-30 20:43:38 -0500

    Initial Commit

---
+++ WordSimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordSimulator
+//
+//  Created by Songhee Yim on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
